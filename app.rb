4ce3adf1e51f271ef125d7e8bafd2ecd66198c61
Mia Henderson <mimato@users.noreply.github.com>
2016-02-25 15:13:00 -0500

    Load config properly, change config to remove redundant db

--- app.rb
+++ app.rb
@@ -1,15 +1,19 @@
 require 'sinatra/base'
 require 'sinatra/activerecord'
+require 'erb'
+require 'sinatra/config_file'
+
 
 # Load models, routes
-Dir[File.join('models', '*.rb')].each { |file| require_relative file }
-Dir[File.join('routes', '*.rb')].each { |file| require_relative file }
-
-CONFIG = YAML.load_file(File.join('config', 'config.yml'))[:environment]
-
 # App is a Sinatra app example
 class App < Sinatra::Base
   register Sinatra::ActiveRecordExtension
+  register Sinatra::ConfigFile
+
+  config_file File.join('config', 'config.yml')
+
+  Dir[File.join('models', '*.rb')].each { |file| require_relative file }
+  Dir[File.join('routes', '*.rb')].each { |file| require_relative file }
 
   configure :production do
     enable :logging
@@ -27,4 +31,12 @@ class App < Sinatra::Base
   before do
     logger.level = 0
   end
+
+  helpers do
+    include Rack::Utils
+    alias_method :h, :escape_html
+    alias_method :u, :escape
+  end
+
+  set :views, File.join(File.dirname(__FILE__), 'views')
 end
